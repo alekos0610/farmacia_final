2a0c82bcd5bc77507653eca09f6de1b096f8671a
sandres.silva <43944987+alekos0610@users.noreply.github.com>
2022-10-14 23:15:33 -0500

    DB2 y documentación
    Se adjunta la segunda base de datos de prueba, y el manual de usuario en formato PDF

---
+++ DB2/Farmaciagalera/capaEntidad/E_Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaEntidad
+{
+    public class E_Producto
+    {
+
+        public string CodigoProducto { get; set; }
+
+        public string NombreProducto { get; set; }
+
+        public double Concentracion { get; set; }
+
+        public double FechaVencimiento { get; set; }
+
+        public double ValorUnitario { get; set; }
+    }
+}
+
---
+++ DB2/Farmaciagalera/capaNegocios/N_Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using capaDatos;
+using capaEntidad;
+using System.Data;
+
+namespace capaNegocios
+{
+    public class N_Producto
+    {
+        D_Producto objdato = new D_Producto();        
+       
+        public DataTable N_listado()
+        {
+            return objdato.D_listado();
+        }
+
+    }
+}
